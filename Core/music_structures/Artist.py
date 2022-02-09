6715ad73f1a75112521438478555c6448c636b77
refaelkdo <69142452+refaelkdo@users.noreply.github.com>
2022-02-09 11:54:53 +0200

    added artist functions

---
+++ Core/music_structures/Artist.py
@@ -0,0 +1,23 @@
+from Core.music_structures.Album import Album
+from Core.music_structures.Track import Track
+
+
+class Artist:
+    def __init__(self, id: str, name: str):
+        self.id = id
+        self.name = name
+        self.albums = list()  # the list of all tracks of this Album
+
+    def add_album(self, album: Album):
+        self.tracks.append(album)
+
+    def if_album_exists(self, album_id: str):
+        for album in self.albums:
+            if album.id == album_id:
+                return True
+        return False
+
+    def add_song_to_album(self, album_id: str, track: Track):
+        for album in self.albums:
+            if album.id == album_id:
+                album.add_song_to_album(track)
