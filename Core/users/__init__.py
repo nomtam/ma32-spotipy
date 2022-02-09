05462a91461e2559260d40e6f8e3c2d9d733d826
refaelkdo <69142452+refaelkdo@users.noreply.github.com>
2022-02-09 19:04:07 +0200

    users signup

--- Core/users/__init__.py
+++ Core/users/__init__.py
@@ -1,6 +1,8 @@
 from Core.IO.File import read_data_from_json_file
 from Monitoring.exceptions import UsernameIsNotExistError
 import Monitoring.logger as logger
+import Monitoring.configurator as config
+
 
 def get_users_data(file_path, function=read_data_from_json_file):
     return function(file_path)
@@ -15,7 +17,11 @@ def login(user: str, password: str, data: dict):
 
 
 def signup(user: str, password: str, data: dict, user_permission: str):
-    if user in data.keys():
-        
+    if user not in data.keys():
+        if user_permission.lower() in config.config_data['USERS_DATA']['PERMISSION_LEVELS']:
+            data[user] = [password, user_permission]
+        else:
+            return "user permission is incorrect. can be one of the three" + config.config_data['USERS_DATA'][
+                'PERMISSION_LEVELS']
     else:
-        return False
+        return "username already in use"
