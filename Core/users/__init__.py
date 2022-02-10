ad1b56e2763649bc1825ac5dc7b1932538f81e67
refaelkdo <69142452+refaelkdo@users.noreply.github.com>
2022-02-10 08:01:24 +0200

    users login and signup comppleted

--- Core/users/__init__.py
+++ Core/users/__init__.py
@@ -1,29 +1,41 @@
 from Core.IO.File import read_data_from_json_file
+from Core.IO.File import json_file_writer
 from Monitoring.exceptions import UsernameIsNotExistError
 import Monitoring.logger as logger
 import Monitoring.configurator as config
 
-
-def get_users_data(file_path, function=read_data_from_json_file):
-    return function(file_path)
+PASSWORD = 0
 
 
-def login(user: str, password: str, data: dict):
-    if user in data.keys():
-        return data[user] == password
-    else:
-        logger.add_error(UsernameIsNotExistError(user))
-        return False
+class UsersData:
+    def __init__(self):
+        self.data = {}
 
+    def get_users_data(self, file_path=config.config_data['USERS_DATA']['USERS_PATH'],
+                       function=read_data_from_json_file):
+        self.data = function(file_path)
 
-def signup(user: str, password: str, data: dict, user_permission: str):
-    if user not in data.keys():
-        if user_permission.lower() in config.config_data['USERS_DATA']['PERMISSION_LEVELS']:
-            data[user] = [password, user_permission]
-            return True
+    def login(self, user: str, password: str):
+        if user in self.data.keys():
+            return self.data[user][PASSWORD] == password
         else:
-            return "user permission is incorrect. can be one of the three" + config.config_data['USERS_DATA'][
-                'PERMISSION_LEVELS']
-    else:
-        logger.add_info("username already in use")
-        return "username already in use"
+            logger.add_error(UsernameIsNotExistError(user))
+            return False
+
+    def signup(self, user: str, password: str, user_permission: str):
+        if user not in self.data.keys():
+            if user_permission.lower() in config.config_data['USERS_DATA']['PERMISSION_LEVELS']:
+                self.data[user] = [password, user_permission]
+                return True
+            else:
+                return "user permission is incorrect. can be one of the three" + config.config_data['USERS_DATA'][
+                    'PERMISSION_LEVELS']
+        else:
+            logger.add_info("username already in use")
+            return "username already in use"
+
+    def save_data(self, file_path=config.config_data['USERS_DATA']['USERS_PATH']):
+        try:
+            json_file_writer(file_path, self.data)
+        except Exception:
+            logger.add_error("problem writing to file")
--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
+
 def main():
     from Core.IO.Dir import read_all_json_files_from_directory
     from Core.music_structures.spotipy_data import SpotipyData
-    from Core.IO.File import read_data_from_json_file
+    from Core.IO.File import json_file_writer
     import Monitoring.configurator as config
-    print(read_data_from_json_file(config.config_data['USERS_DATA']['USERS_PATH']))
+    from Core.users import UsersData
+    data = UsersData()
+    data.get_users_data()
+    print(data.signup("rds", "123", "free"))
+    data.save_data()
 
 
 if __name__ == "__main__":
